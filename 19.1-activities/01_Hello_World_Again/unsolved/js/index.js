b18fd6125ab2fb3207c6abd3951a238e18eb90c2
Erin Cooper <erin.cooper0724@gmail.com>
2021-03-13 17:20:35 -0500

    19.1 activities

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,44 @@
 // Create a variable below
+var helloWorld = 'Hello Friends!';
 
 // Console.log() your variable here.
+console.log (helloWorld);
+
+alert(helloWorld);
+
+var cat1 = 1;
+var cat2 = 1;
+var cat3 = 1;
+
+function addcats () {
+console.log (cat1 + cat2 + cat3);
+}
+addcats();
+
+var x = '3';
+var y = '2';
+var z = x+y;
+
+function addvariables () {
+console.log (x+y);
+}
+addvariables();
+
+
+var x = '3';
+var y = '2';
+var z = 'There are ' + x + y + ' people outside.';
+    
+function addvariables () {
+console.log (z);
+}
+addvariables();
+
+var x = 3;
+var y = 2;
+var z = 'There are ' + (x + y) + ' people outside.';
+    
+function addvariables () {
+console.log (z);
+}
+addvariables();
